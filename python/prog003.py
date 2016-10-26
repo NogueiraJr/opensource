ece9f82b1b6c8817aa44d90613f05c9d82cb085a
NogueiraJr <nogueira@hotmail.com>
2016-10-26 19:16:04 -0200

    Alteracao teste

--- python/prog003.py
+++ python/prog003.py
@@ -34,7 +34,7 @@ def re_hashing( i, code ):
 def insertionsort( aList ):
   for i in range( 1, len( aList ) ):
     tmp = aList[i]
-    k = i
+    k = i 
     while k > 0 and tmp < aList[k - 1]:
         aList[k] = aList[k - 1]
         k -= 1
